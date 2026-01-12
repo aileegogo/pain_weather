1fdda6e761ca0fa0af8fb12e27f476317c38aabb
leesangkyu <aileego1030@gmail.com>
2026-01-12 12:02:33 +0900

    Trigger auto deploy

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,22 +1,16 @@
-class HomeController < ApplicationController
-  def index
-    @city_ko = params[:city].presence || "동탄시"
-    
-    # 1. DB에서 해당 도시의 최신 데이터를 검색
-    @weather = DailyWeather.where(location: @city_ko).last
-    
-    # 2. 데이터가 없으면 API 호출하여 새로 생성
-    if @weather.nil?
-      city_map = { 
-        "동탄시" => "Hwaseong", "서울시" => "Seoul", "부산시" => "Busan", 
-        "대구시" => "Daegu", "제주시" => "Jeju", "남원시" => "Namwon" 
-      }
-      city_en = city_map[@city_ko] || "Seoul"
+def index
+  @city_ko = params[:city].presence || "동탄시"
+  
+  # [수정] 테스트 기간에는 기존 데이터를 찾지 않고 바로 서비스를 실행하여 갱신합니다.
+  city_map = { 
+    "동탄시" => "Hwaseong", "서울시" => "Seoul", "부산시" => "Busan", 
+    "대구시" => "Daegu", "제주시" => "Jeju", "남원시" => "Namwon" 
+  }
+  city_en = city_map[@city_ko] || "Seoul"
 
-      service = DailyPainService.new(city_en, @city_ko)
-      if service.run_daily_process
-        @weather = DailyWeather.where(location: @city_ko).last
-      end
-    end
+  service = DailyPainService.new(city_en, @city_ko)
+  if service.run_daily_process
+    # 새로 생성된 가장 최신 데이터를 가져옵니다.
+    @weather = DailyWeather.where(location: @city_ko).last
   end
 end
