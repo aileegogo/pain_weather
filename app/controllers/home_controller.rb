fb55b1f8f53acff0e367b6053750d97ec55a1f0f
leesangkyu <aileego1030@gmail.com>
2026-01-12 18:41:37 +0900

    Update design and fix ordering

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,13 @@
-class HomeController < ApplicationController
-  def index
-    @city_ko = params[:city].presence || "동탄시"
-    
-    city_map = { 
-      "동탄시" => "Hwaseong", "서울시" => "Seoul", "부산시" => "Busan", 
-      "강릉시" => "Gangneung", "속초시" => "Sokcho", "인천시" => "Incheon",
-      "대구시" => "Daegu", "제주시" => "Jeju"
-    }
-    city_en = city_map[@city_ko] || "Seoul"
+# app/controllers/home_controller.rb
 
-    service = DailyPainService.new(city_en, @city_ko)
-    if service.run_daily_process
-      # [최종 수정] id: :desc 로 변경하여 조회 정확도를 100%로 높입니다.
-      @weather = DailyWeather.where(location: @city_ko).order(id: :desc).first
-    end
+def index
+  @city_ko = params[:city].presence || "동탄시"
+  # ... (기존 매핑 로직 그대로 유지) ...
+
+  service = DailyPainService.new(city_en, @city_ko)
+  if service.run_daily_process
+    # 엔진이 성공했을 때만 데이터를 가져오므로 에러가 날 확률이 극히 낮습니다.
+    @weather = DailyWeather.where(location: @city_ko).order(id: :desc).first
+    @weekly_weather = DailyWeather.where(location: @city_ko).order(id: :desc).limit(7).reverse
   end
 end
