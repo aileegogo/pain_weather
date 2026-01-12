077ce193fe9b283482e415429fd980d9d7ad472b
leesangkyu <aileego1030@gmail.com>
2026-01-12 12:10:27 +0900

    Fix controller syntax error

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,23 @@
-def index
-  @city_ko = params[:city].presence || "동탄시"
-  
-  # [수정] 테스트 기간에는 기존 데이터를 찾지 않고 바로 서비스를 실행하여 갱신합니다.
-  city_map = { 
-    "동탄시" => "Hwaseong", "서울시" => "Seoul", "부산시" => "Busan", 
-    "대구시" => "Daegu", "제주시" => "Jeju", "남원시" => "Namwon" 
-  }
-  city_en = city_map[@city_ko] || "Seoul"
+class HomeController < ApplicationController
+  def index
+    @city_ko = params[:city].presence || "동탄시"
+    
+    # 도시 영어 이름 매핑
+    city_map = { 
+      "동탄시" => "Hwaseong", 
+      "서울시" => "Seoul", 
+      "부산시" => "Busan", 
+      "대구시" => "Daegu", 
+      "제주시" => "Jeju",
+      "남원시" => "Namwon" 
+    }
+    city_en = city_map[@city_ko] || "Seoul"
 
-  service = DailyPainService.new(city_en, @city_ko)
-  if service.run_daily_process
-    # 새로 생성된 가장 최신 데이터를 가져옵니다.
-    @weather = DailyWeather.where(location: @city_ko).last
+    # 무조건 실시간 데이터를 가져오도록 서비스 실행
+    service = DailyPainService.new(city_en, @city_ko)
+    if service.run_daily_process
+      # 방금 생성된 가장 최신 데이터를 DB에서 가져와 화면에 전달
+      @weather = DailyWeather.where(location: @city_ko).last
+    end
   end
 end
